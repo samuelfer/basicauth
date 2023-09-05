dd1e02fddc4f84698151daf878cf29bd2ee9bc23
sfsantos <sfsantos@tce.pb.gov.br>
2023-09-04 21:39:14 -0300

    Encode senha usuário

--- src/main/java/com/marhasoft/basicauth/service/UsuarioService.java
+++ src/main/java/com/marhasoft/basicauth/service/UsuarioService.java
@@ -1,24 +1,46 @@
 package com.marhasoft.basicauth.service;
 
 import com.marhasoft.basicauth.dto.UsuarioDTO;
+import com.marhasoft.basicauth.enums.RoleEnum;
+import com.marhasoft.basicauth.model.Role;
 import com.marhasoft.basicauth.model.Usuario;
 import com.marhasoft.basicauth.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public List<Usuario> getUsuarios() {
         return usuarioRepository.findAll();
     }
 
-    public Usuario cadastrar(Usuario usuario) {
+    public Usuario cadastrar(UsuarioDTO usuarioDTO) {
+        Set<Role> roles = usuarioDTO.getRoles().stream()
+                .map(role -> Role.builder()
+                        .descricao(RoleEnum.valueOf(role))
+                        .build())
+                .collect(Collectors.toSet());
+
+        Usuario usuario = Usuario.builder()
+                .login(usuarioDTO.getLogin())
+                .email(usuarioDTO.getEmail())
+                .senha(passwordEncoder.encode(usuarioDTO.getSenha()))
+                .status(true)
+                .roles(roles)
+                .dataCriacao(LocalDateTime.now())
+                .build();
         return usuarioRepository.save(usuario);
     }
 
